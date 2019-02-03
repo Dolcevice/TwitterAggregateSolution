80ba6bf93e39b4d015d4027f5b04bd02a310d7a8
Dolcevice <Dolcevice@users.noreply.github.com>
2019-02-03 05:48:40 -0600

    Finished Training/result calculation

--- common/twitter_api.py
+++ common/twitter_api.py
@@ -7,15 +7,27 @@ API_KEY_SECRET = 'aWZB24sCd4YmCYJT8EstnPLIJemRinWYQ3SHcR8dnETJlp0RLJ'
 ACCESS_TOKEN = '817167849357615104-sTE4VvShmAbxyVNOFqdngfu8pl1onRT'
 ACCESS_TOKEN_SECRET = '14MlBjPpNUy1IjFpv7RwksgJTOae6DVlac8fNbduY9S9E'
 
-# Setup authorization
-auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+MAX_TWEETS = 100
 
-# Create API
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
 
-search_q = input("Enter the search query: ")
-search_results = api.search(q=search_q)
+def main():
+    # Setup authorization
+    auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-for content in search_results:
-    print(content.text)
+    # Create API
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+
+    search_q = input("Enter the search query: ")
+    search_cursor = tweepy.Cursor(api.search, q=search_q, lang='en').items(MAX_TWEETS)
+    for tweet in search_cursor:
+        with open('data.json', 'a') as file:
+            file.write(json.dumps(tweet._json))
+            file.write("\n")
+
+    # for content in search_results:
+    #    print(content.text)
+
+
+if __name__ == '__main__':
+    main()
